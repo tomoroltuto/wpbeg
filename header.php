db5500136a3d55aff6c5d05bbaf9a81baf6d0657
瀬川 友仁 <to1mo2.to2mo3@gmail.com>
2025-05-14 07:52:11 +0900

    add:header.php の編集

--- header.php
+++ header.php
@@ -1,34 +1,22 @@
 <!DOCTYPE html>
-<html lang="ja">
+<html lang="<?php language_attributes(); ?>">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="format-detection" content="telephone=no">
-    <title>index | wpbeg-static</title>
     <meta name="description" content="WordPress theme development static data for beginners">
     <meta name="keywords" content="WordPress, Theme, development">
-    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/earlyaccess/mplus1p.css">
-    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Sacramento&amp;amp;subset=latin-ext">
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.css">
-    <link rel="stylesheet" href="css/wpbeg.css">
-    <link rel="shortcut icon" href="//wp.yat-net.com/wpbeg-static/images/common/ico/favicon.ico">
+    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/images/common/ico/favicon.ico">
+    <?php wp_head(); ?>
 </head>
 
-<body>
+<body <?php body_class(); ?>>
     <header class="header">
-        <h1 class="header__ttl"><a href="./">WPBEG<span class="header__ttl__sub">static</span></a></h1>
-        <p class="header__descrition">Just another WordPress site</p>
-        <form class="p-search-form" id="searchform">
-            <input class="p-search-form__keyword" placeholder="キーワード">
-            <input class="p-search-form__submit" id="searchsubmit" type="submit" value="検索">
-        </form>
-        <div class="menu-gmenu-container">
-            <ul class="menu">
-                <li class="menu-item current_page_item"><a href="index.html">Blog</a></li>
-                <li class="menu-item"><a href="page.html">About</a></li>
-            </ul>
-        </div>
+        <h1 class="header__ttl"><a href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?><span
+                    class="header__ttl__sub">static</span></a></h1>
+        <p class="header__description"><?php bloginfo('description'); ?></p>
+        <?php get_search_form(); ?>
+        <?php wp_nav_menu(); ?>
     </header>
