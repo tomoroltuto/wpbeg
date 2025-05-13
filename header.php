3923a46599c24c8c2aa8f66d0ac6bea9330a0892
瀬川 友仁 <to1mo2.to2mo3@gmail.com>
2025-05-14 07:40:26 +0900

    add:トップページのテンプレート化

---
+++ header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="format-detection" content="telephone=no">
+    <title>index | wpbeg-static</title>
+    <meta name="description" content="WordPress theme development static data for beginners">
+    <meta name="keywords" content="WordPress, Theme, development">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/earlyaccess/mplus1p.css">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Sacramento&amp;amp;subset=latin-ext">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.css">
+    <link rel="stylesheet" href="css/wpbeg.css">
+    <link rel="shortcut icon" href="//wp.yat-net.com/wpbeg-static/images/common/ico/favicon.ico">
+</head>
+
+<body>
+    <header class="header">
+        <h1 class="header__ttl"><a href="./">WPBEG<span class="header__ttl__sub">static</span></a></h1>
+        <p class="header__descrition">Just another WordPress site</p>
+        <form class="p-search-form" id="searchform">
+            <input class="p-search-form__keyword" placeholder="キーワード">
+            <input class="p-search-form__submit" id="searchsubmit" type="submit" value="検索">
+        </form>
+        <div class="menu-gmenu-container">
+            <ul class="menu">
+                <li class="menu-item current_page_item"><a href="index.html">Blog</a></li>
+                <li class="menu-item"><a href="page.html">About</a></li>
+            </ul>
+        </div>
+    </header>
--- index.php
+++ index.php
@@ -1,114 +1,56 @@
-<!DOCTYPE html>
-<html lang="ja">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="format-detection" content="telephone=no">
-    <title>index | wpbeg-static</title>
-    <meta name="description" content="WordPress theme development static data for beginners">
-    <meta name="keywords" content="WordPress, Theme, development">
-    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/earlyaccess/mplus1p.css">
-    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Sacramento&amp;amp;subset=latin-ext">
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.css">
-    <link rel="stylesheet" href="css/wpbeg.css">
-    <link rel="shortcut icon" href="//wp.yat-net.com/wpbeg-static/images/common/ico/favicon.ico">
-</head>
-
-<body>
-    <header class="header">
-        <h1 class="header__ttl"><a href="./">WPBEG<span class="header__ttl__sub">static</span></a></h1>
-        <p class="header__descrition">Just another WordPress site</p>
-        <form class="p-search-form" id="searchform">
-            <input class="p-search-form__keyword" placeholder="キーワード">
-            <input class="p-search-form__submit" id="searchsubmit" type="submit" value="検索">
-        </form>
-        <div class="menu-gmenu-container">
-            <ul class="menu">
-                <li class="menu-item current_page_item"><a href="index.html">Blog</a></li>
-                <li class="menu-item"><a href="page.html">About</a></li>
-            </ul>
-        </div>
-    </header>
-    <div class="c-wrap">
-        <div class="c-grid">
-            <div class="p-contents">
-                <div class="post">
-                    <h2 class="post__ttl"><a href="single.html">記事タイトル</a></h2>
-                    <ul class="post__meta">
-                        <li class="post__meta__item">
-                            <date class="post__meta__date">2017年10月31日</date>
-                        </li>
-                        <li class="post__meta__item"><i class="fa fa-folder" aria-hidden="true"><a
-                                    class="post__meta__link" href="#">カテゴリ名</a></i></li>
-                        <li class="post__meta__item"><i class="fa fa-tag" aria-hidden="true"><a class="post__meta__link"
-                                    href="#">タグ名</a></i></li>
-                    </ul><img class="attachment-post-thumbnail" src="images/eyecatch/001.png" alt="アイキャッチ">
-                    <p>本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。<a
-                            class="more-link" href="single.html">続きを読む</a></p>
-                </div>
-                <div class="post">
-                    <h2 class="post__ttl"><a
-                            href="single.html">長い記事のタイトル長い記事のタイトル長い記事のタイトル長い記事のタイトル長い記事のタイトル長い記事のタイトル長い記事のタイトル</a></h2>
-                    <ul class="post__meta">
-                        <li class="post__meta__item">
-                            <date class="post__meta__date">2017年10月31日</date>
-                        </li>
-                        <li class="post__meta__item"><i class="fa fa-folder" aria-hidden="true"><a
-                                    class="post__meta__link" href="#">カテゴリ名</a></i></li>
-                        <li class="post__meta__item"><i class="fa fa-tag" aria-hidden="true"><a class="post__meta__link"
-                                    href="#">タグ名</a></i></li>
-                    </ul><img class="attachment-post-thumbnail" src="images/eyecatch/002.png" alt="アイキャッチ">
-                    <p>本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。<a
-                            class="more-link" href="single.html">続きを読む</a></p>
-                </div>
-                <div class="post">
-                    <h2 class="post__ttl"><a href="single.html">記事タイトル</a></h2>
-                    <ul class="post__meta">
-                        <li class="post__meta__item">
-                            <date class="post__meta__date">2017年10月31日</date>
-                        </li>
-                        <li class="post__meta__item"><i class="fa fa-folder" aria-hidden="true"><a
-                                    class="post__meta__link" href="#">カテゴリ名</a></i></li>
-                        <li class="post__meta__item"><i class="fa fa-tag" aria-hidden="true"><a class="post__meta__link"
-                                    href="#">タグ名</a></i></li>
-                    </ul><img class="attachment-post-thumbnail" src="images/eyecatch/003.png" alt="アイキャッチ">
-                    <p>本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。<a
-                            class="more-link" href="single.html">続きを読む</a></p>
-                </div>
-            </div>
-            <ul class="p-pagenation">
-                <li class="prevpostslink"><a rel="next" href="#">Prev</a></li>
-                <li class="nextpostslink"><a rel="next" href="#">Next</a></li>
-            </ul>
-            <div class="p-sidebar">
-                <div class="widget">
-                    <h2><i class="fa fa-folder-open" aria-hidden="true"></i> Category</h2>
-                    <ul>
-                        <li><a href="#">カテゴリ名</a></li>
-                        <li><a href="#">カテゴリ名</a></li>
-                    </ul>
-                </div>
-                <div class="widget">
-                    <h2><i class="fa fa-tags" aria-hidden="true"></i> Tag</h2>
-                    <div class="tagcloud"><a class="tag-cloud-link" href="#">タグ</a><a class="tag-cloud-link"
-                            href="#">タグ</a></div>
-                </div>
-                <div class="widget">
-                    <h2><i class="fa fa-archive" aria-hidden="true"></i> Archive</h2>
-                    <ul>
-                        <li><a href="#">2017年10月</a></li>
-                        <li><a href="#">2017年9月</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
-    <footer class="footer">
-        <p class="footer__copyright"><small>&copy; WPBEG 2017</small></p>
-    </footer>
-</body>
-
-</html>
+<?php get_header(); ?>
+<div class="c-wrap">
+	<div class="c-grid">
+		<div class="p-contents">
+			<div class="post">
+				<h2 class="post__ttl"><a href="single.html">記事タイトル</a></h2>
+				<ul class="post__meta">
+					<li class="post__meta__item">
+						<date class="post__meta__date">2017年10月31日</date>
+					</li>
+					<li class="post__meta__item"><i class="fa fa-folder" aria-hidden="true"><a class="post__meta__link"
+								href="#">カテゴリ名</a></i></li>
+					<li class="post__meta__item"><i class="fa fa-tag" aria-hidden="true"><a class="post__meta__link"
+								href="#">タグ名</a></i></li>
+				</ul><img class="attachment-post-thumbnail" src="images/eyecatch/001.png" alt="アイキャッチ">
+				<p>本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。<a
+						class="more-link" href="single.html">続きを読む</a></p>
+			</div>
+			<div class="post">
+				<h2 class="post__ttl"><a
+						href="single.html">長い記事のタイトル長い記事のタイトル長い記事のタイトル長い記事のタイトル長い記事のタイトル長い記事のタイトル長い記事のタイトル</a></h2>
+				<ul class="post__meta">
+					<li class="post__meta__item">
+						<date class="post__meta__date">2017年10月31日</date>
+					</li>
+					<li class="post__meta__item"><i class="fa fa-folder" aria-hidden="true"><a class="post__meta__link"
+								href="#">カテゴリ名</a></i></li>
+					<li class="post__meta__item"><i class="fa fa-tag" aria-hidden="true"><a class="post__meta__link"
+								href="#">タグ名</a></i></li>
+				</ul><img class="attachment-post-thumbnail" src="images/eyecatch/002.png" alt="アイキャッチ">
+				<p>本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。<a
+						class="more-link" href="single.html">続きを読む</a></p>
+			</div>
+			<div class="post">
+				<h2 class="post__ttl"><a href="single.html">記事タイトル</a></h2>
+				<ul class="post__meta">
+					<li class="post__meta__item">
+						<date class="post__meta__date">2017年10月31日</date>
+					</li>
+					<li class="post__meta__item"><i class="fa fa-folder" aria-hidden="true"><a class="post__meta__link"
+								href="#">カテゴリ名</a></i></li>
+					<li class="post__meta__item"><i class="fa fa-tag" aria-hidden="true"><a class="post__meta__link"
+								href="#">タグ名</a></i></li>
+				</ul><img class="attachment-post-thumbnail" src="images/eyecatch/003.png" alt="アイキャッチ">
+				<p>本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。本文の抜粋が入ります。<a
+						class="more-link" href="single.html">続きを読む</a></p>
+			</div>
+		</div>
+		<ul class="p-pagenation">
+			<li class="prevpostslink"><a rel="next" href="#">Prev</a></li>
+			<li class="nextpostslink"><a rel="next" href="#">Next</a></li>
+		</ul>
+		<?php get_sidebar(); ?>
+	</div>
+</div>
+<?php get_footer(); ?>
