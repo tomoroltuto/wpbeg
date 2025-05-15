9efba4b5a785518600e29ffc115fc0081f76e4de
瀬川 友仁 <to1mo2.to2mo3@gmail.com>
2025-05-16 00:03:48 +0900

    add:フィードの設定

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@
 add_theme_support('menus');
 add_theme_support('title-tag');
 add_theme_support('post-thumbnails');
+add_theme_support('automatic-feed-links');
 
 //タイトル出力
 function wpbeg_title($title)
