24023c7be5eb9c704bae53022fc0261f2127d770
瀬川 友仁 <to1mo2.to2mo3@gmail.com>
2025-05-17 14:42:36 +0900

    fix: テーマチェックの必須・推奨項目の修正

--- header.php
+++ header.php
@@ -8,15 +8,17 @@
     <meta name="format-detection" content="telephone=no">
     <meta name="description" content="WordPress theme development static data for beginners">
     <meta name="keywords" content="WordPress, Theme, development">
-    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/images/common/ico/favicon.ico">
     <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
+    <?php wp_body_open(); ?>
     <header class="header">
         <h1 class="header__ttl"><a href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?><span
                     class="header__ttl__sub">static</span></a></h1>
         <p class="header__description"><?php bloginfo('description'); ?></p>
         <?php get_search_form(); ?>
-        <?php wp_nav_menu(); ?>
+        <?php wp_nav_menu(array(
+            'theme_location' => 'global',
+        )); ?>
     </header>
